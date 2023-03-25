5731364acb9c4787ba9a8e555d77479ed749ff64
Nathan Moes <nathanmoes7@gmail.com>
2023-03-24 18:54:15 -0700

    change layout for text input and button

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text
-        style={{ margin: 10, borderColor: "red", borderWidth: 2, padding: 10 }}
-      >
-        Communism was a mistake! Capitalism will reign!
-      </Text>
-      <Button title="click me" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal!" style={styles.TextInput} />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
